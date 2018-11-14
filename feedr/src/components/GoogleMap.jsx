ec8a3721ae162b7e6ba93eb64d5e22f0a4f833c9
zhangbooming <BoyangZhang2020@u.northwestern.edu>
2018-11-14 12:44:41 -0600

    add google_map to tommy's branch

---
+++ feedr/src/components/GoogleMap.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+
+class GoogleMap extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+        }
+    }
+
+
+    render() {
+
+        const style = {
+            width: '100%',
+            height: '100%'
+        }
+
+
+        console.log(this.props.Lat+"the data received from RestProfile ")
+        return (    
+            <div>
+                <Map
+                    google={this.props.google}
+                    style={style}
+                    initialCenter={{
+                        lat: this.props.Lat,
+                        lng: this.props.Lng
+                    }}
+                    zoom={14}>
+                    <Marker onClick={this.onMarkerClick} name={'Current location'} />
+                    <InfoWindow onClose={this.onInfoWindowClose}>
+                    </InfoWindow>
+                </Map>
+            </div>
+        )
+    }
+
+}
+
+//export default GoogleMap;
+export default GoogleApiWrapper({
+    apiKey: ("AIzaSyBc3bsKQXVsZ-P-MRqPQyIiW1xwi_HEGZw")
+})(GoogleMap)
--- feedr/src/components/RestProfile.jsx
+++ feedr/src/components/RestProfile.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import GoogleMap from './GoogleMap';
 
 class RestProfile extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      displayed: false
+      displayed: false,      
+      mapShowStatus : false
     }
 
   }
@@ -15,6 +17,15 @@ class RestProfile extends Component {
       displayed: false
     })
      {this.props.closeRestProfile()}
+  }
+
+  mapShow(){
+    this.setState({
+      mapShowStatus : true 
+    },function(){
+      console.log(this.state.mapShowStatus)
+    })
+   
   }
 
   cardClass(){
@@ -34,6 +45,10 @@ class RestProfile extends Component {
   }
 
   render() {
+
+    if(this.state.mapShowStatus === false){
+      console.log(this.state.mapShowStatus+"居然可以直接获得这个值了!")
+
     return (
       <div className="rest-profile-container">
         <div className={this.cardClass()} style={{zIndex:'999'}}>
@@ -43,10 +58,19 @@ class RestProfile extends Component {
           <div className="restProfileDescription">Phone: {this.props.restPhone}</div>
           <div className="restProfileDescription">Price: {this.props.restPrice}</div>
           <div className="restProfileDescription">Rating: {this.props.restRating} / 5 stars</div>
+          <button className="button1" onClick={()=>this.mapShow()}>Go</button>
           <button className="button1" onClick={()=>this.changeCardClass()}>CLOSE</button>
         </div>
       </div>
     );
+    }
+
+    return(
+      <div>
+      <GoogleMap Lat={this.props.restLat} Lng={this.props.restLng}></GoogleMap>
+      </div>
+    )
+
   }
 }
 
